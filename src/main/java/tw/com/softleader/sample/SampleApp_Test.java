f9ed4f44dfe37f5bdedec408b374e0e952628028
Yang <Yang@Yang43>
2017-06-13 06:21:41 +0800

    first commit

---
+++ src/main/java/tw/com/softleader/sample/SampleApp_Test.java
@@ -0,0 +1,11 @@
+package tw.com.softleader.sample;
+
+public class SampleApp_Test {
+
+	public static void main(String[] args) {
+		CalculateService calculateService = new CalculateService();
+		System.out.println(calculateService.divide(int3,int5));
+		
+	}
+
+}
