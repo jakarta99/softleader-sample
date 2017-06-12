cda7f4f6a2956b49ce9009a0004da209d11183e9
Gary Lee <jakarta99@gmail.com>
2017-06-12 16:51:03 +0800

    add calculator interfaces

--- src/main/java/tw/com/softleader/sample/CalculateService.java
+++ src/main/java/tw/com/softleader/sample/CalculateService.java
@@ -5,5 +5,36 @@ public class CalculateService {
 	public int sum(int a, int b) {
 		return a+b;
 	}
+
+	// user1
+	public int substract(int a, int b) {
 	
+	}
+	
+	// user2
+	public int multiply(int a, int b) {
+		
+	}
+	
+	// user3
+	public double divide(int a, int b) {
+		
+	}
+	
+	// user4 取餘數
+	public int remainder(int a, int b) {
+		
+	}
+	
+	// user5 平方
+	public int square(int a, int b) {
+		
+	}
+	
+	// user6 三次方
+	public int power3(int a, int b) {
+		
+	}
 }
+
+
