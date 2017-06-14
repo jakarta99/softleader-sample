1bc0ce6e3a1983ed9f6c0b233895d6d312236610
Yang43 <deardamn43@gmail.com>
2017-06-14 21:35:30 +0800

    /

--- src/test/java/tw/com/softleader/sample/CalculateServiceTest.java
+++ src/test/java/tw/com/softleader/sample/CalculateServiceTest.java
@@ -32,7 +32,7 @@ public class CalculateServiceTest {
 	@Test
 	public void testDivide() {
 		CalculateService calculateService = new CalculateService();
-		assertEquals(1,calculateService.divide(1,1,0));
+		assertEquals(1,calculateService.divide(3,3,0));
 		
 		
 	}
