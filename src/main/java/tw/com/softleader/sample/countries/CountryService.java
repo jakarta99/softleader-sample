eb3e7222ab178896cb4fad8fe4b04172e775a7db
Lacerjoe <lacerjoe@gmail.com>
2017-06-14 16:37:20 +0800

    Homework CountryService

---
+++ src/main/java/tw/com/softleader/sample/countries/CountryService.java
@@ -0,0 +1,9 @@
+package tw.com.softleader.sample.countries;
+
+public class CountryService {
+	String[] countries = { "Taiwan", "Thailand", "Japan" };
+
+	public String[] getall() {
+		return countries;
+	}
+}
---
+++ src/test/java/tw/com/softleader/sample/countries/CountryServiceTest.java
@@ -0,0 +1,20 @@
+package tw.com.softleader.sample.countries;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class CountryServiceTest {
+	private void assertEquals(String string, String string2, String string3, String country) {
+	}
+
+	@Test
+	public void countryServiceTest() {
+		CountryService countryService = new CountryService();
+		for (String country : countryService.countries) {
+			assertEquals("Taiwan", "Thailand", "Japan", country);
+		}
+
+	}
+
+}
