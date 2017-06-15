2e981b995245b63c43c64aa857a5f0d99a74ba53
howard <tomx220052@gmail.com>
2017-06-15 17:49:29 +0800

    0549

--- src/test/java/tw/com/softleader/sample/fruit/FruitServiceTest.java
+++ src/test/java/tw/com/softleader/sample/fruit/FruitServiceTest.java
@@ -1,17 +1,20 @@
 package tw.com.softleader.sample.fruit;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class FruitServiceTest {
 	
-	private void assertEquals(String fru, String fru1, String fru2, String fru3, String fru4,String [] fruits) {
-	}
 	@Test
 	public void fruitServiceTest() {
+		String[] compair = {"西瓜","蘋果","芭樂","柳丁","橘子" };
 		FruitService fruitService = new FruitService();
-		for (String fruits : fruitService.getAll()) {
-			assertEquals("西瓜","蘋果","芭樂","柳丁","橘子", fruitService.getAll());
-		}
+		
+		
+			assertEquals(compair, fruitService.getAll());
+		
 	}
+
 
 }
