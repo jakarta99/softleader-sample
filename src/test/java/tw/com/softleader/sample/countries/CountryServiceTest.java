199b8c037dcab91633886417740cee122d211b98
Liu Chun <lacerjoe@gmail.com>
2017-06-15 16:14:06 +0800

    Create CountryServiceTest.java
    修改檢查機制。不要Override assertEquals。

--- src/test/java/tw/com/softleader/sample/countries/CountryServiceTest.java
+++ src/test/java/tw/com/softleader/sample/countries/CountryServiceTest.java
@@ -1,20 +1,15 @@
-package tw.com.softleader.sample.countries;
-
-//import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 //import static org.junit.Assert.fail;
 import org.junit.Test;
-
 public class CountryServiceTest {
-	private void assertEquals(String string, String string2, String string3, String country) {
-	}
+
 
 	@Test
 	public void countryServiceTest() {
 		CountryService countryService = new CountryService();
-		for (String country : countryService.getall()) {
-			assertEquals("Taiwan", "Thailand", "Japan", country);
-		}
-
+			assertEquals("Taiwan", countryService.countries[0]);
+			assertEquals("Taipei", countryService.countries[1]);
+			assertEquals("Japan", countryService.countries[2]);
 	}
 
 }
