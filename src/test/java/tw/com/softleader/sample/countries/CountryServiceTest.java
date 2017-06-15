40710482c79d12262d0cdf7582f4ef92d0602b09
Liu Chun <lacerjoe@gmail.com>
2017-06-15 17:09:33 +0800

    Create CountryServiceTest.java
    不同做法~

--- src/test/java/tw/com/softleader/sample/countries/CountryServiceTest.java
+++ src/test/java/tw/com/softleader/sample/countries/CountryServiceTest.java
@@ -1,15 +1,23 @@
 import static org.junit.Assert.assertEquals;
 //import static org.junit.Assert.fail;
 import org.junit.Test;
+
+// public class CountryServiceTest {
+
+// 	@Test
+// 	public void countryServiceTest() {
+// 		CountryService countryService = new CountryService();
+// 		assertEquals("Taiwan", countryService.countries[0]);
+// 		assertEquals("Taipei", countryService.countries[1]);
+// 		assertEquals("Japan", countryService.countries[2]);
+// 	}
+//}
 public class CountryServiceTest {
-
-
+	@SuppressWarnings("deprecation")
 	@Test
 	public void countryServiceTest() {
+		String[] inputCountries = { "Taiwan", "Thailand", "Japan" };
 		CountryService countryService = new CountryService();
-			assertEquals("Taiwan", countryService.countries[0]);
-			assertEquals("Taipei", countryService.countries[1]);
-			assertEquals("Japan", countryService.countries[2]);
+		assertEquals(inputCountries, countryService.getall());
 	}
-
 }
