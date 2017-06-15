9ba716bea1ea2d9ceb479467d831dbd334b3d42a
howard <tomx220052@gmail.com>
2017-06-15 17:51:50 +0800

    0551

--- src/test/java/tw/com/softleader/sample/fruit/FruitServiceTest.java
+++ src/test/java/tw/com/softleader/sample/fruit/FruitServiceTest.java
@@ -6,12 +6,11 @@ import org.junit.Test;
 
 public class FruitServiceTest {
 	
+	@SuppressWarnings("deprecation")
 	@Test
 	public void fruitServiceTest() {
 		String[] compair = {"西瓜","蘋果","芭樂","柳丁","橘子" };
 		FruitService fruitService = new FruitService();
-		
-		
 			assertEquals(compair, fruitService.getAll());
 		
 	}
