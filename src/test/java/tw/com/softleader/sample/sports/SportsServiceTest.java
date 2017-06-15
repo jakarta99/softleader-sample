866e85ab5fd2d65c628d47e0114f1e5dc9f71427
III-ITC\dynmatic72 <kagemaru106523@gmail.com>
2017-06-15 16:52:10 +0800

    0615 02

--- src/test/java/tw/com/softleader/sample/sports/SportsServiceTest.java
+++ src/test/java/tw/com/softleader/sample/sports/SportsServiceTest.java
@@ -2,23 +2,24 @@ package tw.com.softleader.sample.sports;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 public class SportsServiceTest {
 
-	public void bookServiceTest() {
+	@Test
+	public void sportServiceTest() {
+
 		SportsService sports = new SportsService();
 		String[] checkname = sports.getall();
-		
-		assertEquals("Baseball",  checkname[0]);
-		assertEquals("Tennis",  checkname[1]);
-		assertEquals("Swimming", checkname[2]);
-		assertEquals("Basketball",  checkname[3]);
 
-		
-		}
-//		for (String sport : sports.sports) {
-//
-//		}
+		assertEquals("Baseball", checkname[0]);
+		assertEquals("Tennis", checkname[1]);
+		assertEquals("Swimming", checkname[2]);
+		assertEquals("Basketball", checkname[3]);
 
 	}
+	// for (String sport : sports.sports) {
+	//
+	// }
 
-
+}
