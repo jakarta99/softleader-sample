29fc5c44fdeccc2f4580191fec674c40009725c9
Yang43 <deardamn43@gmail.com>
2017-06-16 18:03:30 +0800

    --

--- src/test/java/tw/com/softleader/sample/games/GamesServiceTesting.java
+++ src/test/java/tw/com/softleader/sample/games/GamesServiceTesting.java
@@ -1,18 +1,19 @@
 package tw.com.softleader.sample.games;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class GameServiceTesting {
-
-	public void GameServiceTest(){
 	@Test
- GameService gamer = new GameService();
-	
-        String games[] = gamer.getall();
-	
-	assertEquals("Detiny2", games[0]);
-	assertEquals("Witcher3",games[1]);
-	assertEquals("Fallout4",games[2]);
-}
+	public void GameServiceTest() {
+
+		GameService gamer = new GameService();
+
+		String games[] = gamer.getall();
+
+		assertEquals("Detiny2", games[0]);
+		assertEquals("Witcher3", games[1]);
+		assertEquals("Fallout4", games[2]);
+	}
 }
