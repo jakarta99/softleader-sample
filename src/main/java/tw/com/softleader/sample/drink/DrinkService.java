df216b8e2590b64006a2cb0841606f112b3c978f
Gary Lee <jakarta99@gmail.com>
2017-06-19 16:46:55 +0800

    ..

--- src/main/java/tw/com/softleader/sample/drink/DrinkService.java
+++ src/main/java/tw/com/softleader/sample/drink/DrinkService.java
@@ -1,5 +1,7 @@
 package tw.com.softleader.sample.drink;
 
+import java.util.Collection;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 import tw.com.softleader.sample.commons.GenericService;
@@ -12,6 +14,36 @@ import tw.com.softleader.sample.commons.GenericService;
  *
  */
 public class DrinkService implements GenericService<Drink> {
+
+	@Override
+	public Drink getOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Drink> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Drink data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Drink data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Drink data) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 	
