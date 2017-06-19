0d9615acdeb7bff2fbc7b4ee12a003f5ddd4d0c0
Gary Lee <jakarta99@gmail.com>
2017-06-19 14:59:17 +0800

    add standard methods

--- src/main/java/tw/com/softleader/sample/commons/GenericService.java
+++ src/main/java/tw/com/softleader/sample/commons/GenericService.java
@@ -2,7 +2,14 @@ package tw.com.softleader.sample.commons;
 
 public interface GenericService {
 
+	public String getOne(int id);
 	
 	public String[] getAll();
+	
+	public void insert(String data);
+	
+	public void update(String data, int id);
+	
+	public void delete(String data);
 	
 }
