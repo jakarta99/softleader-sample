b6c5a69a898c24465e605fa9f23a259a3eaf6888
howard <howard@192.168.1.99>
2017-06-19 15:57:17 +0800

    ...

--- src/main/java/tw/com/softleader/sample/fruit/FruitService.java
+++ src/main/java/tw/com/softleader/sample/fruit/FruitService.java
@@ -1,11 +1,37 @@
 package tw.com.softleader.sample.fruit;
 
-public class FruitService {
-	String[] fruits = {"西瓜","蘋果","芭樂","柳丁","橘子"};
-	
-	public String[] getAll(){
+import java.util.Arrays;
+
+import tw.com.softleader.sample.commons.GenericService;
+
+public class FruitService implements GenericService {
+	private String[] fruits = { "西瓜", "蘋果", "芭樂"};
+
+	public String[] getAll() {
 		return fruits;
-		}
 	}
 
+	@Override
+	public String getOne(int id) {
+		return fruits[id];
+	}
 
+	@Override
+	public void insert(String data) {
+		fruits = Arrays.copyOf(fruits, fruits.length+1);
+		fruits[fruits.length-1] = data;
+		
+	}
+
+	@Override
+	public void update(String data, int id) {
+		fruits[id] = data;
+		
+	}
+
+	@Override
+	public void delete(String data) {
+//		fruits = Arrays.copyOf(fruits, fruits.length-1);
+		
+	}
+}
