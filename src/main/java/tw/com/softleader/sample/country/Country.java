8f3c42ba0ea05d63a1dcd359b7552e5ebcdb6316
Lacerjoe <lacerjoe@gmail.com>
2017-06-20 09:38:08 +0800

    no message

--- src/main/java/tw/com/softleader/sample/country/Country.java
+++ src/main/java/tw/com/softleader/sample/country/Country.java
@@ -1,5 +1,32 @@
 package tw.com.softleader.sample.country;
 
 public class Country {
+	private int id;
+	private String name;
+	private String size;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
 
 }
