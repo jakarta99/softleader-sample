c789d3a3232a775392ff80f63dccf09b65206b59
Yang <Yang@Yang43>
2017-06-21 13:33:10 +0800

    corrected

--- src/main/java/tw/com/softleader/sample/commons/GenericService.java
+++ src/main/java/tw/com/softleader/sample/commons/GenericService.java
@@ -5,13 +5,13 @@ import java.util.List;
 public interface GenericService<T> {
 
 	public T getOne(int id);
-	
+
 	public List<T> getAll();
-	
+
 	public void insert(T data);
-	
+
 	public void update(T data);
-	
+
 	public void delete(int id);
-	
+
 }
