8bb9cc2483f76cb02afe39ddbdf1a8bf8681388d
Yang <Yang@Yang43>
2017-06-21 16:27:49 +0800

    0621

--- src/main/java/tw/com/softleader/sample/games/GameService.java
+++ src/main/java/tw/com/softleader/sample/games/GameService.java
@@ -5,69 +5,57 @@ import java.util.List;
 
 public class GameService implements GenericService<Game> {
 
-		private List<Game> games = new ArrayList<Game>();
-		
-		public GameService() {
+	private List<Game> games = new ArrayList<Game>();
 
-			Game game1 = new Game();
-			game1.setId(1);
-			game1.setName("Destiny2");
-			game1.setType("Fps");
-			
-			Game game2 = new Game();
-			game2.setId(2);
-			game2.setName("Witcher3");
-			game2.setType("Rpg");
-			
-			Game game3 = new Game();
-			game3.setId(3);
-			game3.setName("Fallout4");
-			game3.setType("Rpg");
-			
-			games.add(game1);
-			games.add(game2);
-			games.add(game3);
-		}
-		
-		
-		@Override
-		public Game getOne(int id) {
-			return games.get(id);
-			
-		}
+	public GameService() {
 
-		@Override
-		public List<Game> getAll() {
-			return games;
-		}
+		Game game1 = new Game();
+		game1.setId(1);
+		game1.setName("Destiny2");
+		game1.setType("Fps");
 
-		@Override
-		public void insert(Game data) {
-			games.add(data);
-			
-		}
+		Game game2 = new Game();
+		game2.setId(2);
+		game2.setName("Witcher3");
+		game2.setType("Rpg");
 
-		@Override
-		public void update(Game data) {
-           games.set(3,data);
-		
-		}
+		Game game3 = new Game();
+		game3.setId(3);
+		game3.setName("Fallout4");
+		game3.setType("Rpg");
 
-		@Override
-		public void delete(int id) {
-			games.remove(id);
-			
-		}
+		games.add(game1);
+		games.add(game2);
+		games.add(game3);
+	}
 
+	@Override
+	public Game getOne(int id) {
+		return games.get(id);
 
-		
+	}
 
-	
+	@Override
+	public List<Game> getAll() {
+		return games;
+	}
 
-		
-		
-		
-		
-	
+	@Override
+	public void insert(Game data) {
+		games.add(data);
+
+	}
+
+	@Override
+	public void update(Game data) {
+		games.set(3, data);
+
+	}
+
+	@Override
+	public void delete(int id) {
+		games.remove(id);
+
+	}
 
 }
--- src/test/java/tw/com/softleader/sample/games/GamesServiceTesting.java
+++ src/test/java/tw/com/softleader/sample/games/GamesServiceTesting.java
@@ -1,20 +1,72 @@
 package tw.com.softleader.sample.games;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
 
-public class GamesServiceTesting {
+@SuppressWarnings("unused")
+public class GameServiceTest {
+
+	private GameService gameService = new GameService();//
+
 	@Test
-	public void GameServiceTest() {
+	public void testGetOne() {
+		GameService games = new GameService();
 
-		GameService gamer = new GameService();
-
-		String games[] = gamer.getAll();
-
-		assertEquals("Detiny2", games[0]);
-		assertEquals("Witcher3", games[1]);
-		assertEquals("Fallout4", games[2]);
+		assertEquals("Destiny2", games.getOne(0).getName());
+		assertEquals("Witcher3", games.getOne(1).getName());
+		assertEquals("Fallout4", games.getOne(2).getName());
 	}
+
+	@Test
+	public void testGetAll() {
+
+		GameService games = new GameService();
+
+		assertEquals("Destiny2", games.getOne(0).getName());
+		assertEquals(1, games.getOne(0).getId());
+		assertEquals("Fps", games.getOne(0).getType());
+		assertEquals("Witcher3", games.getOne(1).getName());
+		assertEquals(2, games.getOne(1).getId());
+		assertEquals("Rpg", games.getOne(1).getType());
+		assertEquals("Fallout4", games.getOne(2).getName());
+		assertEquals(3, games.getOne(2).getId());
+		assertEquals("Rpg", games.getOne(2).getType());
+	}
+
+	@Test
+	public void testInsert() {
+
+		int a = 4;
+		String b = "WOW";
+		String c = "MMORPG";
+		Game game4 = new Game();
+		game4.setId(a);
+		game4.setName(b);
+		game4.setType(c);
+		gameService.insert(game4);
+		assertEquals(4, game4.getId());
+		assertEquals(b, game4.getName());
+		assertEquals(c, game4.getType());
+
+	}
+
+	@Test
+	public void testUpdate() {
+		GameService games = new GameService();
+		//
+
+	}
+
+	@Test
+	public void testDelete() {
+		GameService games = new GameService();
+		games.delete(0);
+		assertEquals("Witcher3", games.getOne(0).getName());
+		assertEquals("Rpg", games.getOne(0).getType());
+		assertEquals(2, games.getOne(0).getId());
+	}
+
 }
-//
