11355259915fbb3af95096fb8b3f093592278521
Yang <Yang@Yang43>
2017-06-23 20:36:21 +0800

    0623

--- src/main/java/tw/com/softleader/sample/games/GameService.java
+++ src/main/java/tw/com/softleader/sample/games/GameService.java
@@ -2,8 +2,6 @@ package tw.com.softleader.sample.games;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GameService implements GenericService<Game> {
 
