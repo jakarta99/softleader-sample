6f9e7a1fc5a96a6d5568cc8e69faea74a0ff9686
reiji0713 <dunk800034@gmail.com>
2017-06-30 01:03:27 +0800

    hw temp 2

--- src/main/java/tw/com/softleader/sample/movie/MovieService.java
+++ src/main/java/tw/com/softleader/sample/movie/MovieService.java
@@ -1,6 +1,7 @@
 package tw.com.softleader.sample.movie;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import tw.com.softleader.sample.commons.GenericService;
@@ -9,76 +10,25 @@ public class MovieService implements GenericService<Movie> {
 
 	private List<Movie> movies = new ArrayList<Movie>();
 
-	public MovieService() {
-		Movie movies1 = new Movie();
-		movies1.setId(1);
-		movies1.setName("Captain of America");
-		movies1.setPrice("100");
-
-		Movie movies2 = new Movie();
-		movies2.setId(2);
-		movies2.setName("Silence of the lambs");
-		movies2.setPrice("200");
-
-		Movie movies3 = new Movie();
-		movies3.setId(3);
-		movies3.setName("King Arthor legend of the sword");
-		movies3.setPrice("300");
-
-		Movie movies4 = new Movie();
-		movies4.setId(4);
-		movies4.setName("Crimson Peak");
-		movies4.setPrice("400");
-
-		Movie movies5 = new Movie();
-		movies5.setId(5);
-		movies5.setName("The Godfather");
-		movies5.setPrice("500");
-
-		movies.add(movies1);
-		movies.add(movies2);
-		movies.add(movies3);
-		movies.add(movies4);
-		movies.add(movies5);
-	}
-
 	@Override
-	public Movie getOne(int id) {
-		for (int i = 0; i < movies.size(); i++) {
-			if (id == movies.get(i).getId()) {
-				return movies.get(i);
-			}
-		}
+	public Movie getOne(Long id) {
 		return null;
-
 	}
 
 	@Override
-	public List<Movie> getAll() {
-		return movies;
+	public Collection<Movie> getAll() {
+		return null;
 	}
 
 	@Override
-	public void insert(Movie data) {
-		movies.add(data);
+	public void insert(Movie entity) {
 	}
 
 	@Override
-	public void update(Movie data) {
-		for (int i = 0; i < movies.size(); i++) {
-			if (data.getId() == movies.get(i).getId()) {
-				movies.set(i, data);
-			}
-		}
-
+	public void update(Movie entity) {
 	}
 
 	@Override
-	public void delete(int id) {
-		for (int i = 0; i < movies.size(); i++) {
-			if (id == movies.get(i).getId()) {
-				movies.remove(i);
-			}
-		}
+	public void delete(Long id) {
 	}
 }
--- src/test/java/tw/com/softleader/sample/movie/MovieServiceTest.java
+++ src/test/java/tw/com/softleader/sample/movie/MovieServiceTest.java
@@ -2,6 +2,7 @@ package tw.com.softleader.sample.movie;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Test;
@@ -9,58 +10,12 @@ import org.junit.Test;
 public class MovieServiceTest {
 
 	private MovieService movieService = new MovieService();
-	List<Movie> movies = movieService.getAll();
 
 	@Test
 	public void testGetAll() {
-
-		assertEquals(movies.get(0).getId(), 1);
-		assertEquals(movies.get(0).getName(), "Captain of America");
-		assertEquals(movies.get(0).getPrice(), "100");
-		assertEquals(movies.get(1).getId(), 2);
-		assertEquals(movies.get(1).getName(), "Silence of the lambs");
-		assertEquals(movies.get(1).getPrice(), "200");
-		assertEquals(movies.get(2).getId(), 3);
-		assertEquals(movies.get(2).getName(), "King Arthor legend of the sword");
-		assertEquals(movies.get(2).getPrice(), "300");
-		assertEquals(movies.get(3).getId(), 4);
-		assertEquals(movies.get(3).getName(), "Crimson Peak");
-		assertEquals(movies.get(3).getPrice(), "400");
-		assertEquals(movies.get(4).getId(), 5);
-		assertEquals(movies.get(4).getName(), "The Godfather");
-		assertEquals(movies.get(4).getPrice(), "500");
-	}
-
-	@Test
-	public void testGetOne() {
-		Movie movies = movieService.getOne(2);
-		assertEquals(movies.getId(), 2);
-		assertEquals(movies.getName(), "Silence of the lambs");
-		assertEquals(movies.getPrice(), "200");
-	}
-
-	@Test
-	public void testInsertUpdateDelete() {
-		Movie movie1 = new Movie();
-		movie1.setId(6);
-		movie1.setName("Transformers:The Last Knight");
-		movie1.setPrice("600");
-		movieService.insert(movie1);
-		assertEquals(movies.get(5).getId(), 6);
-		assertEquals(movies.get(5).getName(), "Transformers:The Last Knight");
-		assertEquals(movies.get(5).getPrice(), "600");
-
-		Movie movie2 = new Movie();
-		movie2.setId(5);
-		movie2.setName("The Godfather2");
-		movie2.setPrice("550");
-		movieService.update(movie2);
-		assertEquals(movies.get(4).getId(), 5);
-		assertEquals(movies.get(4).getName(),"The Godfather2");
-		assertEquals(movies.get(4).getPrice(),"550");
-
-		Movie movie3 = movieService.getOne(1);
-		movieService.delete(1);
-		assertEquals(movies.contains(movie3), false);
+		Collection<Movie> movies = movieService.getAll();
+		for(Movie movie:movies){
+			System.out.println(movie);
+		}
 	}
 }
