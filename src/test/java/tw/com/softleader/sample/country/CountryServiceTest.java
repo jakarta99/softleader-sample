e6fb5df58f6bef7adb06f237e006e80d6d13e6e0
III-ITC\Student EEIT9616 <lacerjoe@gmail.com>
2017-07-05 09:06:26 +0800

    Removed connection on CountryServiceTest

--- src/test/java/tw/com/softleader/sample/country/CountryServiceTest.java
+++ src/test/java/tw/com/softleader/sample/country/CountryServiceTest.java
@@ -1,17 +1,9 @@
 package tw.com.softleader.sample.country;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Collection;
-
-import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
-
-import tw.com.softleader.sample.commons.DataSourceUtil;
 
 public class CountryServiceTest {
 
@@ -21,25 +13,7 @@ public class CountryServiceTest {
 
 	@Test
 	public void testGetOne() {
-		try {
-			DataSource ds = DataSourceUtil.getInstance().getDataSource();
-			Connection connection = ds.getConnection();
-
-			Statement stmt = connection.createStatement();
-			String sqlCmd = "SELECT id FROM country ORDER BY RANDOM() LIMIT 1";
-			ResultSet rs = stmt.executeQuery(sqlCmd);
-			if (rs.next()) {
-				log.info("2:testGetOne-->" + countryService.getOne(rs.getLong("id")));
-			} else {
-				log.info("2:testGetOne,DB has no data");
-			}
-			rs.close();
-			stmt.close();
-			connection.close();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		log.info("2:testGetOne-->" + countryService.getOne(5L));
 	}
 
 	@Test
