2c79450eea0c15813d95d9f4965e573c2a86b617
diablossv <diablossv09@gmail.com>
2017-07-10 23:01:47 +0800

    Benny homework

---
+++ src/test/java/tw/com/softleader/sample/car/JPersonServiceTest.java
@@ -0,0 +1,78 @@
+package tw.com.softleader.sample.car;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tw.com.softleader.sample.car.Car;
+import tw.com.softleader.sample.car.CarService;
+
+public class JPersonServiceTest {
+	
+	private Logger log = LoggerFactory.getLogger(JPersonServiceTest.class);
+	
+	private JPersonService jPersonService = new JPersonService();
+
+	@Test
+	public void testCrud() {
+		
+		log.info(" insert start"); 
+		JPerson jPerson = new JPerson();
+		jPerson.setIdNo("J00001");
+		jPerson.setName("Benny");
+		
+		Collection<Car> cars = new ArrayList<>();
+		Car car1 = new Car();
+		car1.setBrand("Lamborghini");
+		car1.setName("diablo");
+		car1.setColor("Yellow");
+		cars.add(car1);
+		
+		Car car2 = new Car();
+		car2.setBrand("Mercedes-Benz");
+		car2.setName("G65 AMG");
+		car2.setColor("Sliver");
+		cars.add(car2);
+		jPerson.setCars(cars);
+		jPersonService.insert(jPerson);
+		log.info(" insert end"); 
+		
+		//update
+		log.info(" update start"); 
+		Long newJPersonId = jPerson.getId();
+		Long updateCarId = car2.getId();
+		jPerson.setIdNo("J00002");
+		car2.setColor("Black");
+		
+		jPersonService.update(jPerson);
+		
+		JPerson dbJPerson = jPersonService.getOne(newJPersonId);
+		
+		assertEquals("J00002", dbJPerson.getIdNo());
+		
+		Collection<Car> dbCars = dbJPerson.getCars();
+		for (Car car : dbCars) {
+			if(updateCarId.equals(car.getId())){
+				assertEquals("Black", car.getColor());
+			}
+		}
+		log.info(" update end"); 
+		
+		//delete
+		log.info(" delete start"); 
+		jPersonService.delete(newJPersonId);
+		assertNull(jPersonService.getOne(newJPersonId));
+		CarService carService = new CarService();
+		dbCars = carService.findByJPersonId(newJPersonId);
+		Assert.assertTrue(dbCars.size()  == 0);
+		log.info(" delete end"); 
+	}
+
+}
