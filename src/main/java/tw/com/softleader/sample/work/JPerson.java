3e8c0350d64ac5efb23899d0aa727eb7d9851089
feyalin <faye239563@gmail.com>
2017-07-11 01:00:40 +0800

    JerryLin作業

---
+++ src/main/java/tw/com/softleader/sample/work/JPerson.java
@@ -0,0 +1,64 @@
+package tw.com.softleader.sample.work;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by JerryLin on 2017/7/9.
+ */
+public class JPerson {
+
+
+
+
+    private Long id;
+    private String name;
+    private Long idno;
+    List<Work> works ;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getIdno() {
+        return idno;
+    }
+
+    public List<Work> getWorks() {
+        return works;
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "JPerson{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", idno=" + idno +
+                ", works=" + works +
+                '}';
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public void setIdno(Long idno) {
+        this.idno = idno;
+    }
+
+    public void setWorks(List<Work> works) {
+        this.works = works;
+    }
+}
---
+++ src/test/java/tw/com/softleader/sample/work/WorkServiceTest.java
@@ -0,0 +1,49 @@
+package tw.com.softleader.sample.work;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tw.com.softleader.sample.drink.DrinkServiceTest;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jerry.lin on 2017/7/9.
+ */
+public class WorkServiceTest {
+
+    private Logger log = LoggerFactory.getLogger(DrinkServiceTest.class);
+
+    private WorkService service = new WorkService();
+
+    @Test
+    public void crud () {
+        Collection<Work> works = service.findAll();
+        int originalSize = works.size();
+
+        final Work work1 = new Work();
+        work1.setName("first work");
+
+        service.insert(work1);
+        Long generatedId = work1.getId();
+
+        work1.setName("sec work");
+
+        service.update(work1);
+
+        final Work dbEntity = service.findOne(generatedId);
+
+        assertEquals("sec work", dbEntity.getName());
+
+        service.delete(generatedId);
+
+        works = service.findAll();
+
+        int finalSize = works.size();
+
+        assertEquals(originalSize, finalSize);
+    }
+
+}
