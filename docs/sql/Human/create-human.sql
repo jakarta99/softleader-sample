4bf218e463051817bfd844b94089a43cbf9a64c1
Samuel <Samueltai0730@gmail.com>
2017-07-11 15:01:10 +0800

    HomeWork

---
+++ docs/sql/Human/create-human.sql
@@ -0,0 +1,5 @@
+create table human (
+	id	   bigserial,
+	name   varchar(100),
+	gender  varchar(10)
+)
---
+++ src/test/java/tw/com/softleader/sample/human/HumanServiceTest.java
@@ -0,0 +1,54 @@
+package tw.com.softleader.sample.human;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HumanServiceTest {
+
+	private Logger log = LoggerFactory.getLogger(HumanServiceTest.class);
+	
+	private HumanService humanService = new HumanService();
+
+	@Test
+	public void testCrud() {
+		Collection<Human> humans = humanService.getAll();
+		int originalSize = humans.size();
+		
+		Human humanA = new Human();
+		humanA.setName("A");
+		humanA.setGender("BOY");
+		
+		humanService.insert(humanA);
+		log.debug("{}", humanA);
+		
+		Long generatedId = humanA.getId(); 
+		
+		
+		humanA.setGender("GIRL");
+		humanService.update(humanA);
+		
+		
+		Human entity = humanService.getOne(generatedId);
+		
+		assertEquals("A", entity.getName());
+		
+		assertEquals("GIRL", entity.getGender());
+		
+		
+		humanService.delete(generatedId);
+		assertNull(humanService.getOne(generatedId));
+		
+		humans = humanService.getAll();
+		int finalSize = humans.size();
+		
+		assertEquals(originalSize, finalSize);
+		
+	}
+
+}
