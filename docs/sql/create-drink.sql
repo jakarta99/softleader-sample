a23084ea7547c69588b9330da9079d28f7f7606c
Gary Lee <jakarta99@gmail.com>
2017-07-11 20:22:31 +0800

    drink and d_person tables

--- docs/sql/create-drink.sql
+++ docs/sql/create-drink.sql
@@ -1,5 +1,15 @@
+drop table drink;
 create table drink (
 	id	   bigserial,
+	p_id   bigint,
 	name   varchar(100),
 	color  varchar(50)
+);
+
+drop table d_person;
+create table d_person (
+	id bigserial,
+	name varchar(100),
+	idno varchar(20)
 )
+
