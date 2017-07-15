1eefe62e64929b6b9396294f9c84a5ed9007ff97
sagisawa\kagemaru <kagemaru106523@gmail.com>
2017-07-15 17:58:27 +0800

    一對多三層結構作業 07/15

--- src/main/java/tw/com/softleader/sample/sports/SComDao.java
+++ src/main/java/tw/com/softleader/sample/sports/SComDao.java
@@ -11,11 +11,14 @@ import java.util.Iterator;
 
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
+
 import tw.com.softleader.sample.commons.DataSourceUtil;
 import tw.com.softleader.sample.commons.GenericDao;
 
 public class SComDao implements GenericDao<SCom> {
 
+	private Logger log = Logger.getLogger(this.getClass());
 	Collection<Sport> sportlist = null; // new ArrayList<Sport>();
 	Collection<SPerson> spersonlist = null; // new ArrayList<SPerson>();
 	Collection<SCom> scomlist = null; // new ArrayList<SCom>();
@@ -45,20 +48,26 @@ public class SComDao implements GenericDao<SCom> {
 				scom = new SCom();
 				scom.setId(rsCom.getLong("id"));
 				scom.setName(rsCom.getString("name"));
+				spersonlist = new ArrayList<SPerson>();
 
-				pstmt = connection.prepareStatement(sqlCmdperson);
-				pstmt.setLong(1, id);
-				ResultSet rsPerson = pstmt.executeQuery();
+				log.debug("DAO: findOne SComName = " + rsCom.getString("NAME"));
+
+				PreparedStatement pstmt2 = connection.prepareStatement(sqlCmdperson);
+				pstmt2.setLong(1, id);
+				ResultSet rsPerson = pstmt2.executeQuery();
 				while (rsPerson.next()) {
-					sperson = new SPerson();
+					SPerson sperson = new SPerson();
 					sperson.setComid(rsCom.getLong("id"));
 					sperson.setId(rsPerson.getLong("id"));
 					sperson.setIdnum(rsPerson.getString("idnum"));
 					sperson.setName(rsPerson.getString("name"));
+					sportlist = new ArrayList<Sport>();
 
-					pstmt = connection.prepareStatement(sqlCmdsport);
-					pstmt.setLong(1, rsPerson.getLong("id"));
-					ResultSet rsSport = pstmt.executeQuery();
+					log.debug("DAO: findOne personName = " + rsPerson.getString("name"));
+
+					PreparedStatement pstmt3 = connection.prepareStatement(sqlCmdsport);
+					pstmt3.setLong(1, rsPerson.getLong("id"));
+					ResultSet rsSport = pstmt3.executeQuery();
 
 					while (rsSport.next()) {
 						sport = new Sport();
@@ -67,19 +76,19 @@ public class SComDao implements GenericDao<SCom> {
 						sport.setName(rsSport.getString("name"));
 						sport.setPeople(rsSport.getString("people"));
 
-						sportlist = new ArrayList<Sport>();
-						spersonlist = new ArrayList<SPerson>();
+						log.debug("DAO: findOne sportName = " + rsSport.getString("name"));
 
 						sportlist.add(sport);
-						sperson.setSports(sportlist);
-						spersonlist.add(sperson);
-						scom.setSperson(spersonlist);
 
-						
-						
+					}
+					rsSport.close();
 
-					}rsSport.close();
-				}rsPerson.close();
+					sperson.setSports(sportlist);
+
+					spersonlist.add(sperson);
+				}
+				rsPerson.close();
+				scom.setSperson(spersonlist);
 			}
 
 			rsCom.close();
@@ -97,62 +106,19 @@ public class SComDao implements GenericDao<SCom> {
 	@Override
 	public Collection<SCom> findAll() {
 
-		String sqlCmdcom = "select * from SCom ";
-		String sqlCmdperson = "select * from SPerson ";
-		String sqlCmdsport = "select * from sport ";
+		String sqlCmdcom = "select id from SCom ";
 		DataSource datasource = DataSourceUtil.getInstance().getDataSource();
-
+		scomlist = new ArrayList<SCom>();
 		try {
-
 			Connection connection = datasource.getConnection();
 
 			PreparedStatement pstmt = connection.prepareStatement(sqlCmdcom);
 
 			ResultSet rsCom = pstmt.executeQuery();
 
-			if (rsCom.next()) {
-				scom = new SCom();
-				scom.setId(rsCom.getLong("id"));
-				scom.setName(rsCom.getString("name"));
-
-				pstmt = connection.prepareStatement(sqlCmdperson);
-
-				ResultSet rsPerson = pstmt.executeQuery();
-				while (rsPerson.next()) {
-					sperson = new SPerson();
-					sperson.setComid(rsCom.getLong("id"));
-					sperson.setId(rsPerson.getLong("id"));
-					sperson.setIdnum(rsPerson.getString("idnum"));
-					sperson.setName(rsPerson.getString("name"));
-
-					pstmt = connection.prepareStatement(sqlCmdsport);
-
-					ResultSet rsSport = pstmt.executeQuery();
-
-					while (rsSport.next()) {
-						sport = new Sport();
-						sport.setPersonid(rsPerson.getLong("id"));
-						sport.setId(rsSport.getLong("id"));
-						sport.setName(rsSport.getString("name"));
-						sport.setPeople(rsSport.getString("people"));
-
-						sportlist = new ArrayList<Sport>();
-						spersonlist = new ArrayList<SPerson>();
-						scomlist = new ArrayList<SCom>();
-
-						sportlist.add(sport);
-						sperson.setSports(sportlist);
-						spersonlist.add(sperson);
-						scom.setSperson(spersonlist);
-						scomlist.add(scom);
-
-						
-
-					}
-					rsSport.close();
-						
-				}
-				rsPerson.close();
+			while (rsCom.next()) {
+				SCom tempscom = SComDao.this.findOne(rsCom.getLong("id"));
+				scomlist.add(tempscom);
 			}
 
 			rsCom.close();
@@ -171,65 +137,67 @@ public class SComDao implements GenericDao<SCom> {
 	public void insert(SCom entity) {
 		String sqlCmdcom = "INSERT INTO SCom (name) VALUES (?) ";
 		String sqlCmdperson = "INSERT INTO SPerson (name,idnum,comid) VALUES (?,?,?) ";
-		String sqlCmdsport = "INSERT INTO SPerson (name,people,personid) VALUES (?,?,?) ";
+		String sqlCmdsport = "INSERT INTO sport (name,people,personid) VALUES (?,?,?) ";
 		DataSource datasource = DataSourceUtil.getInstance().getDataSource();
-
+		Long comid = null;
+		Long personid = null;
 		try {
 
 			Connection connection = datasource.getConnection();
-
 			PreparedStatement pstmt = connection.prepareStatement(sqlCmdcom, Statement.RETURN_GENERATED_KEYS);
-
 			pstmt.setString(1, entity.getName());
-
 			pstmt.executeUpdate();
 			ResultSet keySetCom = pstmt.getGeneratedKeys();
 
 			if (keySetCom.next()) {
-				entity.setId(keySetCom.getLong("id"));
+				comid = keySetCom.getLong("id");
+				entity.setId(comid);
 			}
 
 			Iterator<SPerson> spersonindex = entity.getSperson().iterator();
 			while (spersonindex.hasNext()) {
-				sperson = spersonindex.next();
+				SPerson insertSperson = spersonindex.next();
 				pstmt = connection.prepareStatement(sqlCmdperson, Statement.RETURN_GENERATED_KEYS);
-
-				pstmt.setString(1, sperson.getName());
-				pstmt.setString(2, sperson.getIdnum());
-				pstmt.setLong(3, keySetCom.getLong("id"));
-
+				pstmt.setString(1, insertSperson.getName());
+				pstmt.setString(2, insertSperson.getIdnum());
+				pstmt.setLong(3, comid);
 				pstmt.executeUpdate();
 				ResultSet keySetPerson = pstmt.getGeneratedKeys();
 
-				sperson.setId(keySetPerson.getLong("id"));
-				sperson.setComid(keySetCom.getLong("id"));
 				// entity.getSperson().iterator().next().setId(keySetPerson.getLong("id"));
 				// entity.getSperson().iterator().next().setComid(keySetCom.getLong("id"));
 
 				if (keySetPerson.next()) {
+					personid = keySetPerson.getLong("id");
+					insertSperson.setId(personid);
+					insertSperson.setComid(keySetCom.getLong("id"));
+				}
 
-					Iterator<Sport> sportindex = sperson.getSports().iterator();
+				Iterator<Sport> sportindex = insertSperson.getSports().iterator();
 
-					while (sportindex.hasNext()) {
-						sport = sportindex.next();
+				while (sportindex.hasNext()) {
+					Sport insertSport = sportindex.next();
 
-						pstmt = connection.prepareStatement(sqlCmdsport, Statement.RETURN_GENERATED_KEYS);
-						pstmt.setString(1, sport.getName());
-						pstmt.setString(2, sport.getPeople());
-						pstmt.setLong(3, keySetPerson.getLong("id"));
-						pstmt.executeUpdate();
+					pstmt = connection.prepareStatement(sqlCmdsport, Statement.RETURN_GENERATED_KEYS);
+					pstmt.setString(1, insertSport.getName());
+					pstmt.setString(2, insertSport.getPeople());
+					pstmt.setLong(3, personid);
+					pstmt.executeUpdate();
 
-						ResultSet keySetSport = pstmt.getGeneratedKeys();
-
-						sport.setId(keySetSport.getLong("id"));
-						sport.setPersonid(keySetPerson.getLong("id"));
+					ResultSet keySetSport = pstmt.getGeneratedKeys();
+					Long sport_Id = null;
+					if (keySetSport.next()) {
+						sport_Id = keySetSport.getLong("id");
+						insertSport.setId(sport_Id);
+						insertSport.setPersonid(personid);
 						// entity.getSperson().iterator().next().getSports().iterator().next().setId(keySetSport.getLong("id"));
 						// entity.getSperson().iterator().next().getSports().iterator().next().setPersonid(keySetPerson.getLong("id"));
-						
-keySetSport.close();
 					}
-						
-				}keySetPerson.close();
+
+					keySetSport.close();
+				}
+
+				keySetPerson.close();
 			}
 
 			keySetCom.close();
@@ -259,7 +227,6 @@ keySetSport.close();
 		String sqlCmdcom = " DELETE from SCom WHERE id = ?";
 		String sqlCmdperson = "DELETE from SPerson WHERE comid = ?";
 		String sqlCmdsport = "DELETE from sport WHERE personid = ?";
-		String sqlCmdsearch = " SELECT id from SCom WHERE personid = ?";
 		boolean persondeleteflag = false;
 		boolean sportdeleteflag = false;
 
@@ -268,37 +235,39 @@ keySetSport.close();
 		try {
 
 			Connection connection = datasource.getConnection();
+			PreparedStatement pstmt = connection.prepareStatement(sqlCmdsport);
 
-			PreparedStatement pstmt = connection.prepareStatement(sqlCmdsearch);
-			pstmt.setLong(1, id);
+			Iterator<SPerson> personDeleteIndex = SComDao.this.findOne(id).getSperson().iterator();
 
-			ResultSet rssearch = pstmt.executeQuery();
-			if (rssearch.next()) {
-
-				pstmt = connection.prepareStatement(sqlCmdsport);
-
-				pstmt.setLong(1, rssearch.getLong("id"));
+			while (personDeleteIndex.hasNext()) {
+				Long delId = personDeleteIndex.next().getSports().iterator().next().getPersonid();
+				log.debug("delete sport id (person id) = " + delId);
+//				Iterator<Sport> sportDeleteFlag = personDeleteIndex.next().getSports().iterator();
+				pstmt.setLong(1, delId);
 				pstmt.executeUpdate();
 				sportdeleteflag = true;
+				//				log.debug("delete sport = "+sportDeleteFlag.next().getName());
+//				if (sportDeleteFlag.hasNext()) {
+//				}
+			}
+			if (sportdeleteflag) {
+				pstmt = connection.prepareStatement(sqlCmdperson);
 
-				if (sportdeleteflag) {
-					pstmt = connection.prepareStatement(sqlCmdperson);
+				pstmt.setLong(1, id);
+				pstmt.executeUpdate();
+				log.debug("delete person id  = " + id);
+				
+				persondeleteflag = true;
+				if (persondeleteflag) {
+					pstmt = connection.prepareStatement(sqlCmdcom);
 
 					pstmt.setLong(1, id);
 					pstmt.executeUpdate();
-					persondeleteflag = true;
-					if (persondeleteflag) {
-						pstmt = connection.prepareStatement(sqlCmdcom);
+					log.debug("delete com id  = " + id);
 
-						pstmt.setLong(1, id);
-						pstmt.executeUpdate();
-
-					}
 				}
-
 			}
 
-			rssearch.close();
 			pstmt.close();
 			connection.close();
 
