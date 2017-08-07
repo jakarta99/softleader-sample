cb45e9fee3e50cd27bb524386f4c7061ece1c014
hilda.chang <hilda.chang@softleader.com.tw>
2017-08-07 22:49:23 +0800

    add wish object

---
+++ src/main/java/tw/com/triplei/protal/entity/Wish.java
@@ -0,0 +1,17 @@
+package tw.com.triplei.protal.entity;
+
+/**
+ * 許願池
+ */
+public class Wish {
+
+	private Long id;
+	
+	private String image1; // 商品圖片
+	
+	private String wishType;
+	
+	private String brand;
+	
+	private int weekUseCount; // 累積許願池使用次數
+}
